897fcb95c43a8ac6a0c40f7c00345e47a276dff8
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-10-11 20:48:43 -0400

    javascript pseudocode

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+// Dependencies (DOM)
+var APIKey = "84901325de4dfa2e90aa5903c666c0dc"
+var city;
+var queryURL
+
+
+
+
+// Data
+
+
+
+
+
+// User interaction
+// user will type in a city name
+// when user click on search button then the city card will appear below the search button
+// user will then see the city and the date. below will have current temp, wind speed, and humidity.
+// user will also see the next 5 day forecast for that city
+
+
+
+
+
+// Initilization
+
+
+
+
+
+
+
