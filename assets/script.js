f5377cb7d621b3b21209ad4863b28086f49eff25
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-10-11 22:23:38 -0400

    localstorage to save city

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Dependencies (DOM)
 var APIKey = "84901325de4dfa2e90aa5903c666c0dc"
-var city;
+// var city = ""
 var queryURL
 
 
@@ -13,8 +13,18 @@ var queryURL
 
 
 // User interaction
-// user will type in a city name
+// user will type in a city name in the search bar
 // when user click on search button then the city card will appear below the search button
+$(".searchButton").click(function){
+    
+}
+// save the city when refreshed
+for (var i = 0; i < localStorage.length; i++){
+    var city = localStorage.getItem(i);
+    console.log(city)
+    var cityList = $("#cityList").addClass("cityListItems");
+    cityList.append("<li>" + city + "</li>");
+}
 // user will then see the city and the date. below will have current temp, wind speed, and humidity.
 // user will also see the next 5 day forecast for that city
 
